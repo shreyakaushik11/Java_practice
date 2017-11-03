3248694d0f196ae1fb878964a08b4c47db4f8a34
Shreya Kaushik <shreyakaushik11@gmail.com>
2017-11-03 10:23:14 +0530

    Add files via upload

---
+++ BlockTest.java
@@ -0,0 +1,11 @@
+class BlockTest{
+public static void main(String args[]){
+int x;
+int y = 20;
+for(x = 0; x < 10; x++) {
+    System.out.println("The value of x is :" + x);
+	System.out.println("The value of y is :" + y);
+	y = y - 2;
+	}
+}
+}
