c09bfc650426429d36238a33d78f557c8aa6319f
Shreya Kaushik <shreyakaushik11@gmail.com>
2017-11-03 10:24:31 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,10 @@
+class Example 
+{
+public static void main(String args[]){
+int num;
+num = 100;
+System.out.println(num);
+num = num * 2;
+System.out.println("The value of num*2 is:" + num);
+}
+}
