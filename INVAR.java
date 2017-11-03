db502827705f89af12ce2d2973830d84a0c484fe
Shreya Kaushik <shreyakaushik11@gmail.com>
2017-11-03 10:27:57 +0530

    Add files via upload

---
+++ INVAR.java
@@ -0,0 +1,11 @@
+/* Instance Variable */
+class INVAR
+{ 
+  int a = 100;
+  int b = 200;
+  public static void main(String args[])
+  {
+  INVAR x = new INVAR();
+  System.out.println(x.a + x.b );
+  }
+}
